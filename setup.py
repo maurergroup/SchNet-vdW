6bc49fcb69f7270238fe4a2a97a61bf400968bf7
juliawestermayr <julia.westermayr@univie.ac.at>
2021-06-03 14:32:30 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     email='julia.westermayr@warwick.ac.uk',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    scripts=[],
+    scripts=['./scripts/run_ml_pred_new.py','./scripts/run_ml_pred.py','./spk_vdw/dftdisp.py','./spk_vdw/dispcorr.py','./spk_vdw/mbd.py','./spk_vdw/spk_vdw_qbc_interface.py','./spk_vdw/spk_vdw_interface.py','./spk_vdw/__init__.py','./spk_vdw/mbdio.py','./spk_vdw/qmme.py']
     python_requires='>=3.6',
     install_requires=[
         "torch>=0.4.1",
